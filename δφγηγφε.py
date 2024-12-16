8ecbf81bd8744613b09f7762a397960de89a6946
panagiotasmpiliri <sbiliripenny@gmail.com>
2024-12-16 13:19:03 +0200

    g

--- δφγηγφε.py
+++ δφγηγφε.py
@@ -7,7 +7,7 @@ class MyApp(QDialog):
         super().__init__()
         # Φόρτωση του .ui αρχείου
         loadUi("untitled.ui", self)
-        self.Buttonstylesheet=self.load_stylesheet("Buttonstyle.txt")
+ 
         # Σύνδεση του κουμπιού buttonTableMeli με τη λειτουργία
         self.buttonTableMeli.clicked.connect(self.show_table)
 
@@ -17,19 +17,69 @@ class MyApp(QDialog):
 
         # Δημιουργία του κουμπιού επιστροφής
         self.backButton = QPushButton("Επιστροφή")
-        self.backButton.setStyleSheet(self.Buttonstylesheet)
+        self.backButton.setStyleSheet("""
+QPushButton {
+    font-size: 12px;
+    padding: 1px 2.7px;
+    font-weight: 500;
+    background: #598284;
+    color: white;
+    border: none;
+    position: relative;
+    overflow: hidden;
+    border-radius: 2px;
+    cursor: pointer;
+}
+
+QPushButton::hover {
+    background: #3f5b5d;
+}
+
+QPushButton::pressed {
+    transform: scale(0.97);
+}
+
+QPushButton .gradient {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    border-radius: 2px;
+    margin-top: -0.25em;
+    background-image: linear-gradient(
+        rgba(0, 0, 0, 0),
+        rgba(0, 0, 0, 0),
+        rgba(0, 0, 0, 0.3)
+    );
+}
+
+QPushButton .label {
+    position: relative;
+    top: -1px;
+}
+
+QPushButton .transition {
+    transition-timing-function: cubic-bezier(0, 0, 0.2, 1);
+    transition-duration: 500ms;
+    background-color: #3f5b5d;
+    border-radius: 9999px;
+    width: 0;
+    height: 0;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
+
+QPushButton:hover .transition {
+    width: 14em;
+    height: 14em;
+}
+""")
         self.backButton.clicked.connect(self.go_back)
 
-    def load_stylesheet(self, style):
-        try:
-            # Ανάγνωση του αρχείου CSS και φόρτωση στο πρόγραμμα
-            with open(style, "r") as f:
-                stylesheet = f.read()
-                print("stylesheet")
-                return stylesheet  # Επιστρέφουμε το περιεχόμενο του stylesheet
-        except Exception as e:
-            print(f"Error loading stylesheet: {e}")
-            return None
+    
     def show_table(self):
         # Αν ο πίνακας υπάρχει ήδη, αφαιρούμε τον
         if self.table_shown:
